59dc317f8d5f55512477db8458197a465c066fa3
martin <martin.ennemoser@xemedo.com>
2021-09-22 14:45:58 +0200

    Removed unused method.

--- src/ast_builder.py
+++ src/ast_builder.py
@@ -6,28 +6,9 @@ class ASTBuilder:
         self.parser = parser
 
     def parse(self, text):
-        text = self.remove_description(text)
         text = self.remove_comments(text)
         result = self.parser.parseString(text)
         self.post_process(result)
-
-    def remove_description(self, text):
-        descriptionStartComment = "(*@KEY@:DESCRIPTION*)"
-        descriptionEndComment = "(*@KEY@:END_DESCRIPTION*)"
-
-        descriptionStart = text.find(descriptionStartComment)
-        descriptionEnd = text.find(descriptionEndComment)
-        if descriptionStart == -1 or descriptionEnd == -1:
-            return text
-
-        initToDelete = descriptionStart + len(descriptionStartComment)
-        endToDelete = descriptionEnd
-
-        subString1 = text[:initToDelete]
-        subString2 = text[endToDelete:-1]
-
-        text = subString1 + subString2
-        return text
 
     def remove_comments(self, text):
         import re
@@ -36,7 +17,7 @@ class ASTBuilder:
 
         return res
 
-    def post_process(self):
+    def post_process(self, parsed_text):
         """
         Redeclare method types
         :return:
