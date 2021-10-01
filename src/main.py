f541afc3fdfa447f910f0ed1a173af0b93204f9f
martin <martin.ennemoser@xemedo.com>
2021-10-01 18:24:21 +0200

    Improved main.py.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from src.ast_builder import ASTBuilder
 from src.vector_generation import create_similarity_vector, create_occurrence_list
 
 data_path = os.path.join(os.environ.get("DATA_PATH"))
+originalpath = os.path.join(data_path, "original")
 registry_path = os.path.join(data_path, "registry")
 non_parsable_counter = 0
 
@@ -19,15 +20,20 @@ def read_registry(data_path):
 
 registry = read_registry(data_path)
 
-def get_st_file(filenumber):
+def get_clone_file(filenumber):
     path_to_file = os.path.join(data_path, "originalandclones", "clones", registry[filenumber]["Filename"].strip())
     with open(path_to_file) as f:
         return f.read()
 
+def get_registry_file(filename):
+    path_to_file = os.path.join(registry_path, "TRAINED_" + filename + ".txt")
+    with open(path_to_file) as f:
+        return f.readlines()
+
 def add_datapoint(filenumber, creator, tokens_first, label, X, y):
     global non_parsable_counter
     print("Parsing second file: {} ({})".format(registry[filenumber]["Filename"].strip(), registry[filenumber]["#ID"].strip()))
-    second_file = get_st_file(filenumber)
+    second_file = get_clone_file(filenumber)
     try:
         tokens_second = creator.parse(second_file)
     except Exception as e:
@@ -38,42 +44,47 @@ def add_datapoint(filenumber, creator, tokens_first, label, X, y):
     X.append(sim_vector)
     y.append(np.array([label]))
 
+
 def main():
-    # read all files from registry.csv
-  #  registry = read_registry(data_path)
-    onlyfiles = [os.path.join(registry_path, f) for f in os.listdir(registry_path) if os.path.isfile(os.path.join(registry_path, f)) and ".csv" not in f]
+    # get all original files
+    originalfiles = [f for f in os.listdir(originalpath) if os.path.isfile(os.path.join(originalpath, f)) and ".csv" not in f]
 
     creator = ASTBuilder()
     X = []
     y = []
 
-    for file in onlyfiles:
-        with open(file) as file:
-            content = file.readlines()
-            file_number_first = content[0].split(" ")[3][1:]
+    for originalfile in originalfiles:
+        content = get_registry_file(originalfile)
+        file_number_first = content[0].split(" ")[3][1:]
+        first_file = get_clone_file(file_number_first)
+        tokens_first = creator.parse(first_file)
 
-            clones = content[1].replace("Clones:", "").split(",")
-            if len(clones) == 1:
-                clones = []
+#todo extract to method
+#############################
+        clones = content[1].replace("Clones:", "").split(",")
+        clones.remove("\n")
+        if len(clones) == 1:
+            clones = []
 
-            non_clones = content[2].replace("Non Clones:", "").split(",")
-            if len(non_clones) == 1:
-                non_clones = []
+        for clone in clones:
+            clone_nr = clone.strip()
+            if file_number_first == clone_nr:
+                continue
+            add_datapoint(clone.strip(), creator, tokens_first, 1, X, y)
 
-            first_file = get_st_file(file_number_first)
-            tokens_first = creator.parse(first_file)
+#############################
+        non_clones = content[2].replace("Non Clones:", "").split(",")
+        non_clones.remove("\n")
+        if len(non_clones) == 1:
+            non_clones = []
 
-            for clone in clones:
-                clone_nr = clone.strip()
-                if file_number_first == clone_nr:
-                    continue
-                add_datapoint(clone.strip(), creator, tokens_first, 1, X, y)
+        for non_clone in non_clones:
+            non_clone_nr = non_clone.strip()
+            if file_number_first == non_clone_nr:
+                continue
+            add_datapoint(non_clone_nr, creator, tokens_first, 0, X, y)
 
-            for non_clone in non_clones:
-                non_clone_nr = non_clone.strip()
-                if file_number_first == non_clone_nr:
-                    continue
-                add_datapoint(non_clone_nr, creator, tokens_first, 0, X, y)
+        print("Finished file: {}".format(originalfile))
 
 if __name__ == "__main__":
     main()
