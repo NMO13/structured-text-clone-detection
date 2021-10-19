47b201e5e41d7fb0eee763f275a7ab7a93e4a7a1
martin <martin.ennemoser@xemedo.com>
2021-10-19 16:31:47 +0200

    Added html templates.

--- api/app/upload.py
+++ api/app/upload.py
@@ -15,7 +15,7 @@ def allowed_file(filename):
 @bp.route('/', methods=("POST", "GET"))
 def upload():
     if request.method == "GET":
-        return render_template("upload/fileupload.html", error=False)
+        return render_template("upload/base.html", error=False)
     if request.method == 'POST':
         try:
             if request.values["submit"] == "Upload Files":
@@ -24,17 +24,17 @@ def upload():
                 compare_files = request.files.getlist("file[]")
                 [check_file(x) for x in compare_files]
         except:
-            return render_template("upload/fileupload.html", error=True)
+            return render_template("upload/base.html", error=True)
 
         # file is valid
         from src.nn.network_functions import predict
         from . import net
-        sim_vectors = create_similarity_vectors(basefile, compare_files)
+        sim_vectors, filenames = create_similarity_vectors(basefile, compare_files)
         logit, probability = predict(net, sim_vectors)
+        rounded_probs = torch.round(probability).cpu().data.numpy()
+        res = zip(rounded_probs, filenames)
 
-
-
-        return render_template("upload/fileupload.html", error=False, upload_success=True, result=torch.round(probability).cpu().data.numpy())
+        return render_template("upload/result.html", error=False, upload_success=True, basefile=basefile.filename, result=res)
 
 def check_file(file):
     if not (file and allowed_file(file.filename)):
@@ -45,11 +45,13 @@ def create_similarity_vectors(basefile, compare_files):
     creator = ASTBuilder()
     tokens_base = creator.parse(basefile.read().decode("utf-8"))
     sim_vectors = []
+    filenames = []
 
     for compare_file in compare_files:
         print("comparison file: " + compare_file.filename)
         vector = create_similarity_vector(create_occurrence_list(tokens_base),
                                  create_occurrence_list(creator.parse(compare_file.read().decode("utf-8"))))
         sim_vectors.append(vector)
+        filenames.append(compare_file.filename)
 
-    return sim_vectors
+    return sim_vectors, filenames
