e5ea8b5f479e2ecd4bd41530271ac2983000bdf2
martin <martin.ennemoser@xemedo.com>
2021-11-08 16:00:29 +0100

    Added probability threshold slider.

--- api/upload.py
+++ api/upload.py
@@ -52,7 +52,7 @@ def upload():
             expl_ratio = expl_ratio.tolist()
             pca_result = [df["pca-one"].to_list(), df["pca-two"].to_list()]
 
-        return render_template("upload/result.html", error=False, upload_success=True, basefile=basefile.filename, pred_time=pred_time, overall_time=sum(exec_times), basefile_size=utf8len(basetext), result=nn_result, expl_ratio_data=expl_ratio, pca_result=pca_result)
+        return render_template("upload/result.html", error=False, upload_success=True, basefile=basefile.filename, pred_time=pred_time, overall_time=sum(exec_times), basefile_size=utf8len(basetext), result=nn_result, expl_ratio_data=expl_ratio, pca_result=pca_result, probabilities=probabilities.cpu().data.tolist())
 
 
 def utf8len(s):
